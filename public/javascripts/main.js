6a727fe537eaa3aa7a338cc83dbc57c3d6b1a246
baoxiaoying <791736879@qq.com>
2016-09-30 14:16:01 +0800

    react 学习

---
+++ public/javascripts/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default class ActiveBody extends React.Component {
+  render() {
+    return (
+      <div>
+        sb
+      </div>
+    )
+  }
+};
+
+ReactDOM.render(
+  <ActiveBody />
+  , document.getElementById('main'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+/**
+ * Created by wanli on 16/8/4.
+ */
+var path = require('path');
+var webpack = require('webpack');
+var hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
+module.exports = {
+  entry: {
+    index:[path.resolve(__dirname,"./public/javascripts/main.js"), hotMiddlewareScript]
+  },
+  output: {
+    path: path.resolve(__dirname, "./disk/js/"),
+    filename: '[name].js',
+    publicPath: 'http://127.0.0.1:3000/disk'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.less','.css', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /.css?$/,
+        loader: 'style!css'
+      },
+      {
+        test: /.less?$/,
+        loader: 'style!css!less!'
+      },
+      {
+        test: /\.png$|\.jpg$|\.gif$|\.ico$/,
+        loader: "file?name=/public/**/images/[name].[ext]?[hash]"
+      }
+    ]
+  },
+};
